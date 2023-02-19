c2af194a02b9bba7749b217b342ba86a679e4597
MarwanTarekZaky <marwantarekzaky@gmail.com>
2023-02-20 00:25:08 +0200

    under v1.01
    teachers and footer

--- doctor/home/assignment.php
+++ doctor/home/assignment.php
@@ -52,7 +52,7 @@ if (isset($_SESSION['id']) && isset($_SESSION['name'])) {
                 <div class="navbar-nav font-weight-bold mx-auto py-0">
                     <a href="index.php" class="nav-item nav-link">الصفحه الرئيسيه</a>
                     <a href="../registration_and_login/delete_user.php" class="nav-item nav-link">حذف مستخدم</a>
-                    <a href="gallery.php" class="nav-item nav-link">صور المركز</a>
+                    
                     <a href="logout.php" class="nav-item nav-link">تسجيل الخروج</a>
                     <a href="../registration_and_login/login.php" class="nav-item nav-link">تسجيل الدخول</a>
                    <!--<a href="../registration_and_login/signup.php" class="nav-item nav-link">انشاء حساب</a>-->
--- doctor/home/logout.php
+++ doctor/home/logout.php
@@ -56,7 +56,7 @@ header("Location: ../registration_and_login/login.php");
                 <div class="navbar-nav font-weight-bold mx-auto py-0">
                     <a href="index.html" class="nav-item nav-link">الصفحه الرئيسيه</a>
                     <a href="../registration_and_login/delete_user.php" class="nav-item nav-link">حذف مستخدم</a>
-                    <a href="gallery.html" class="nav-item nav-link">صور المركز</a>
+                   
                     <a href="logout.php" class="nav-item nav-link">تسجيل الخروج</a>
                     <a href="../registration_and_login/login.php" class="nav-item nav-link">تسجيل الدخول</a>
                     <!-- <a href="../registration_and_login/signup.php" class="nav-item nav-link">انشاء حساب</a> -->
